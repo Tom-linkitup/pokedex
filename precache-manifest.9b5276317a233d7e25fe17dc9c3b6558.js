086d92fa8536dc4afee1c4d603014d284f4666c3
Tom <tom@TomdeMacBook-Air.local>
2020-06-18 13:35:22 +0800

    Updates

---
+++ precache-manifest.9b5276317a233d7e25fe17dc9c3b6558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685cbff1680509678965e0286ebc993e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7812aff25e06af7ca94c",
+    "url": "/pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2292121d53f2a02b7ec0",
+    "url": "/pokedex/static/css/main.53070e12.chunk.css"
+  },
+  {
+    "revision": "7812aff25e06af7ca94c",
+    "url": "/pokedex/static/js/2.6badc8ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.6badc8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2292121d53f2a02b7ec0",
+    "url": "/pokedex/static/js/main.3d9f3f99.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  }
+]);
